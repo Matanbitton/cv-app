63e3b26cec13a7fe92bbb6b793e9aabe198a9b5f
Matan <matanbitton2@gmail.com>
2022-08-11 21:11:21 +0300

    make state change title onclick event

--- src/Section.js
+++ src/Section.js
@@ -1,28 +1,65 @@
+import React, { useState } from "react";
+import Input from "./Input";
+
 export default function Section(props) {
+  const [isInput, setInput] = useState(false);
+  const [text, setText] = useState(props.title);
+
+  function handleClick(e) {
+    e.preventDefault();
+    setInput((prev) => !prev);
+  }
+  function handleText(event) {
+    setText(() => event.target.value);
+  }
+
   return (
     <div>
-      <h2 className="font-bold text-sky-500">{props.title}</h2>
+      {!isInput && (
+        <h2 onClick={handleClick} className="font-bold text-lg text-sky-500">
+          {text}
+        </h2>
+      )}
+      {isInput && (
+        <Input
+          handleInput={handleText}
+          handleClick={handleClick}
+          placeholder={text}
+        />
+      )}
       {props.type === "Info" && (
         <div>
-          <p>{props.name}</p>
-          <p>{props.email}</p>
-          <p>{props.phone}</p>
-          <p>{props.gitHub}</p>
+          <p className="font-bold text-sky-500 text-center text-2xl">
+            {props.name}
+          </p>
+          <div className="flex gap-2 text-sm justify-center">
+            <p>{props.email}</p>
+            <p>{props.phone}</p>
+            <p>{props.gitHub}</p>
+          </div>
         </div>
       )}
       {(props.type === "Education" || props.type === "Work") && (
-        <div>
+        <div className="text-sm flex  justify-between">
           <p>{props.position}</p>
-          <p>{props.startDate}</p>
-          <p>{props.endDate}</p>
+          <div className="flex gap-2">
+            <p>{props.startDate} -</p>
+            <p> {props.endDate}</p>
+          </div>
         </div>
       )}
       {props.type === "Projects" && (
-        <div>
+        <div className="text-sm">
           <p></p>
         </div>
       )}
-      <p>{props.description}</p>
+      {props.type === "Skills" && (
+        <div className="text-sm bottom-1">
+          <p></p>
+        </div>
+      )}
+
+      <p className="text-sm">{props.description}</p>
     </div>
   );
 }
