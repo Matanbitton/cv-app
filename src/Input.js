67d3b80f279732adeeb6105ad06ab11c1c8d3967
Matan <matanbitton2@gmail.com>
2022-08-13 13:33:12 +0300

    fix bug

--- src/Input.js
+++ src/Input.js
@@ -2,13 +2,13 @@ export default function Input(props) {
   return (
     <form className="flex gap-1 ">
       <input
-        className="rounded-md  focus:outline-sky-500"
+        className="rounded-md  focus:outline-sky-500 w-24"
         onChange={props.handleInput}
         placeholder={props.placeholder}
       ></input>
       <button
         onClick={props.handleClick}
-        className="text-white bg-sky-500 w-6 rounded-md "
+        className="text-white bg-sky-500 w-5 rounded-md "
         type="Submit"
       >
         +
