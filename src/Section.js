a2b53314b72d2c2576cf2bf6f17fd701739f824b
Matan <matanbitton2@gmail.com>
2022-08-13 13:32:54 +0300

    add functionality to remove and add skill

--- src/Section.js
+++ src/Section.js
@@ -1,10 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Input from "./Input";
+import uniqid from "uniqid";
 
-export default function Section(props) {
+export default function Section({
+  title,
+  description,
+  startDate,
+  endDate,
+  name,
+  type,
+  gitHub,
+  email,
+  phone,
+  position,
+  skills,
+}) {
   const [isInput, setInput] = useState(false);
-  const [text, setText] = useState(props.title);
+  const [text, setText] = useState(title);
+  const [nameCV, setName] = useState(name);
+  const [buttonPress, setButtonPress] = useState(false);
 
+  const [skillsCV, setSkillsCV] = useState(
+    skills ? skillsToElements(skills) : ""
+  );
+  const [skill, setSkill] = useState("");
+
+  function getUniqID() {
+    return uniqid();
+  }
+
+  function skillsToElements(skills) {
+    return skills.map((skill) => (
+      <p
+        id={getUniqID()}
+        onClick={handleRemoveSkill}
+        className="bg-slate-400 text-white rounded-md px-2 text-md"
+      >
+        {skill}
+      </p>
+    ));
+  }
   function handleClick(e) {
     e.preventDefault();
     setInput((prev) => !prev);
@@ -12,54 +47,121 @@ export default function Section(props) {
   function handleText(event) {
     setText(() => event.target.value);
   }
+  function handleRemoveSkill(e) {
+    setSkillsCV((prevSkills) =>
+      prevSkills.filter((skill) => {
+        return skill.props.id !== e.target.id;
+      })
+    );
+  }
+  function handleSkill(e) {
+    e.preventDefault();
+    setSkill(() => e.target.value);
+  }
+  useEffect(() => {
+    if (buttonPress === true) {
+      setSkillsCV((prev) => [
+        ...prev,
+        <p
+          onClick={handleRemoveSkill}
+          id={getUniqID()}
+          className="bg-slate-400 text-white rounded-md px-2 text-md"
+        >
+          {skill}
+        </p>,
+      ]);
+      setButtonPress((prevButton) => !prevButton);
+    }
+  }, [buttonPress]);
+
+  function handleName(e) {
+    e.preventDefault();
+    setInput((prev) => !prev);
+  }
+  function handleNameText(event) {
+    setName(() => event.target.value);
+  }
+
+  function handleAddButton() {
+    console.log({ skillsCV });
+    const newSkill = (
+      <Input
+        handleInput={handleSkill}
+        handleClick={(e) => {
+          e.preventDefault();
+          setButtonPress((prev) => !prev);
+
+          setSkillsCV((prev) => [...prev.slice(0, -1)]);
+        }}
+      ></Input>
+    );
+    setSkillsCV((prevSkills) => [...prevSkills, newSkill]);
+  }
 
   return (
-    <div>
-      {!isInput && (
-        <h2 onClick={handleClick} className="font-bold text-lg text-sky-500">
-          {text}
-        </h2>
-      )}
-      {isInput && (
-        <Input
-          handleInput={handleText}
-          handleClick={handleClick}
-          placeholder={text}
-        />
-      )}
-      {props.type === "Info" && (
-        <div>
-          <p className="font-bold text-sky-500 text-center text-2xl">
-            {props.name}
-          </p>
+    <div className="p-2 hover:bg-slate-200 cursor-pointer">
+      {type === "Info" && (
+        <div className=" border-sky-500 border-b-2 ">
+          {!isInput && (
+            <p
+              onClick={handleName}
+              className="font-bold text-sky-500 text-center flex justify-center text-3xl border-sky-500  hover:bg-slate-300"
+            >
+              {nameCV}
+            </p>
+          )}
+          {isInput && (
+            <div className="flex justify-center">
+              <Input
+                handleInput={handleNameText}
+                handleClick={handleName}
+                placeholder={text}
+              />
+            </div>
+          )}
           <div className="flex gap-2 text-sm justify-center">
-            <p>{props.email}</p>
-            <p>{props.phone}</p>
-            <p>{props.gitHub}</p>
+            <p className="hover:bg-slate-300">{email}</p>
+            <p className="hover:bg-slate-300">{phone}</p>
+            <p className="hover:bg-slate-300">{gitHub}</p>
           </div>
         </div>
       )}
-      {(props.type === "Education" || props.type === "Work") && (
+      {(type === "Education" || type === "Work") && (
         <div className="text-sm flex  justify-between">
-          <p>{props.position}</p>
+          <div>
+            <h2 className="font-bold text-lg text-sky-500 ">{text}</h2>
+            <p className="hover:bg-slate-300">{position}</p>
+          </div>
           <div className="flex gap-2">
-            <p>{props.startDate} -</p>
-            <p> {props.endDate}</p>
+            <p className="hover:bg-slate-300">{startDate} -</p>
+            <p className="hover:bg-slate-300"> {endDate}</p>
           </div>
         </div>
       )}
-      {props.type === "Projects" && (
+      {type === "Projects" && (
         <div className="text-sm">
-          <p></p>
+          <h2 className="font-bold text-lg text-sky-500 ">{text}</h2>
+          <p className="hover:bg-slate-300"></p>
         </div>
       )}
-      {props.type === "Skills" && (
-        <div className="text-sm bottom-1">
-          <p></p>
+      {type === "Skills" ? (
+        <div>
+          <h2 className="font-bold text-lg text-sky-500 ">{text}</h2>
+          <div className="flex gap-2 flex-wrap ">
+            {skillsCV}
+            <button
+              onClick={handleAddButton}
+              className="bg-sky-500 text-white px-2 rounded-md text-md"
+            >
+              +
+            </button>
+          </div>
         </div>
+      ) : (
+        ""
       )}
 
-      <p className="text-sm">{props.description}</p>
+      <p className="text-sm hover:bg-slate-300">{description}</p>
     </div>
   );
 }
