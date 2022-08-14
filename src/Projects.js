a94999227b9998db44de123daa0a36858b618ca7
Matan <matanbitton2@gmail.com>
2022-08-14 15:10:45 +0300

    add styling

--- src/Projects.js
+++ src/Projects.js
@@ -1,8 +1,8 @@
 export default function Projects({ text, description, id, handleRemove }) {
   return (
-    <div id={id} handleRemove={handleRemove} className="text-sm">
-      <h2 className="font-bold text-lg text-sky-500 ">{text}</h2>
-      <p className="hover:bg-slate-300">{description}</p>
+    <div id={id} onDoubleClick={handleRemove} className="text-sm">
+      <h2 className="font-bold text-lg text-sky-500 max-w-fit">{text}</h2>
+      <p className="hover:bg-slate-300 max-w-fit">{description}</p>
     </div>
   );
 }
