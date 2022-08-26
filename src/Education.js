71b1b45989b51ba4e7e23a7296bfdf5745d7dd41
Matan <matanbitton2@gmail.com>
2022-08-26 17:03:27 +0300

    fix width problem caused by adding a button

--- src/Education.js
+++ src/Education.js
@@ -5,20 +5,23 @@ export default function Education({
   startDate,
   endDate,
   id,
+  description,
 }) {
   return (
     <div
       id={id}
       onDoubleClick={handleRemove}
-      className="text-sm flex  justify-between"
+      className="text-sm flex justify-between w-9/12"
     >
       <div>
         <h2 className="font-bold text-lg text-sky-500 ">{text}</h2>
-        <p className="hover:bg-slate-300">{position}</p>
-      </div>
-      <div className="flex gap-2  max-h-4 max-w-fit">
-        <p className="hover:bg-slate-300">{startDate} -</p>
-        <p className="hover:bg-slate-300"> {endDate}</p>
+        <div className="flex gap-2  max-h-4 justify-space">
+          <p className="hover:bg-slate-300">{position}</p>
+          <p className="hover:bg-slate-300">{startDate} -</p>
+          <p className="hover:bg-slate-300"> {endDate}</p>
+        </div>
+
+        <p className="hover:bg-slate-300">{description}</p>
       </div>
     </div>
   );
