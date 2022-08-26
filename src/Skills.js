c0b52fe81dc9229ebb189838c5c7b5a7c26fccab
Matan <matanbitton2@gmail.com>
2022-08-26 16:59:23 +0300

    fix aligment issue caused by not specfied width

--- src/Skills.js
+++ src/Skills.js
@@ -6,13 +6,13 @@ export default function Skills({
   id,
 }) {
   return (
-    <div onDoubleClick={handleRemove} id={id}>
+    <div className="group w-9/12" onDoubleClick={handleRemove} id={id}>
       <h2 className="font-bold text-lg text-sky-500 max-w-fit">{text}</h2>
-      <div className="flex gap-2 flex-wrap max-w-fit">
+      <div className="flex gap-2 flex-wrap ">
         {skillsCV}
         <button
           onClick={handleAddButton}
-          className="bg-sky-500 text-white px-2 rounded-md text-md"
+          className="hidden group-hover:block bg-sky-500 text-white px-2 rounded-md text-md"
         >
           +
         </button>
