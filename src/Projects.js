53494b5e977ab1698546eee5cb53ea2c4de6d6be
Matan <matanbitton2@gmail.com>
2022-08-27 19:43:17 +0300

    make delete button aligment

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,6 @@
 export default function Projects({ text, description, id, handleRemove }) {
   return (
-    <div id={id} onDoubleClick={handleRemove} className="text-sm">
+    <div id={id} onDoubleClick={handleRemove} className="text-sm w-9/12">
       <h2 className="font-bold text-lg text-sky-500 max-w-fit">{text}</h2>
       <p className="hover:bg-slate-300 max-w-fit">{description}</p>
     </div>
