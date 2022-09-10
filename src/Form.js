065592a5861460d0ffef78cfe149a34abb644861
Matan <matanbitton2@gmail.com>
2022-09-10 16:56:53 +0300

    add file

---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Form() {
+  return (
+    <div className="w-full h-full">
+      <form>
+        <input>Email</input>
+      </form>
+    </div>
+  );
+}
