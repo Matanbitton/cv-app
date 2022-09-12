63c06e82c5e0450543bcdf101914abe7ffb27f23
Matan <matanbitton2@gmail.com>
2022-09-12 14:26:30 +0300

    add summary section toggle feature

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import PersonalInfo from "./PersonalInfo";
 
 export default function Form() {
   const [personalInfo, setPersonalInfo] = useState({
@@ -9,54 +10,52 @@ export default function Form() {
     email: "",
     linkedinLink: "",
     websiteLink: "",
+    summarySection: "",
   });
-  console.log(personalInfo);
+  const [showSummay, setShowSummary] = useState(false);
 
   function handlePersonalInfoChange(event) {
     setPersonalInfo((prevForm) => {
       return { ...prevForm, [event.target.name]: event.target.value };
     });
+    console.log(personalInfo);
+  }
+  function handleSummary() {
+    setShowSummary((prevShow) => !prevShow);
   }
 
   return (
     <div className="w-full h-full">
-      <form>
-        <input
-          onChange={handlePersonalInfoChange}
-          type="text"
-          placeholder="First Name"
-          name="firstName"
-        ></input>
-        <input
-          onChange={handlePersonalInfoChange}
-          type="text"
-          placeholder="Last Name"
-          name="lastName"
-        ></input>
-        <input
-          onChange={handlePersonalInfoChange}
-          type="tel"
-          placeholder="Phone Number"
-          name="phoneNumber"
-        ></input>
-        <input
-          onChange={handlePersonalInfoChange}
-          type="email"
-          placeholder="Email"
-          name="email"
-        ></input>
-        <input
-          onChange={handlePersonalInfoChange}
-          type="text"
-          placeholder="Linkedin Link"
-          name="linkedinLink"
-        ></input>
-        <input
-          onChange={handlePersonalInfoChange}
-          type="text"
-          placeholder="Personal Website / Github Link"
-          name="websiteLink"
-        ></input>
+      <form className="">
+        <fieldset className="flex flex-col gap-2 ">
+          <h1 className="text-sky-500 text-2xl font-bold text-left pl-6 py-2">
+            Personal Information
+          </h1>
+
+          <PersonalInfo
+            handlePersonalInfoChange={handlePersonalInfoChange}
+            personalInfo={personalInfo}
+          />
+          <div className="flex gap-2 justify-center">
+            <label for="summary">Add Summary Section?</label>
+            <input
+              id="summary"
+              type="checkbox"
+              value="summary"
+              onChange={handleSummary}
+            ></input>
+          </div>
+          {showSummay && (
+            <div className="flex justify-center">
+              <textarea
+                value={personalInfo.summarySection}
+                name="summarySection"
+                onChange={handlePersonalInfoChange}
+                className=" h-24 w-[80%] p-2"
+              ></textarea>
+            </div>
+          )}
+        </fieldset>
       </form>
     </div>
   );
