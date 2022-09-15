408e7701f7e30837fa438eba41e0098292608d69
Matan <matanbitton2@gmail.com>
2022-09-15 10:29:18 +0300

    add labels for each input

--- src/WorkExp.js
+++ src/WorkExp.js
@@ -3,12 +3,25 @@ import React from "react";
 export default function WorkExp() {
   return (
     <div className="flex flex-col">
-      <div className="flex justify-center items-center w-[80%]">
-        <label for="company">Company:</label>
-        <input type="text" id="company"></input>
-
-        <label for="position">Position:</label>
-        <input type="text" id="position"></input>
+      <div className="flex justify-evenly">
+        <div className="flex flex-col">
+          <label for="company">Company:</label>
+          <input
+            type="text"
+            className="rounded   pl-2 shadow border border-slate-400 focus:border-sky-500 focus:outline-none"
+            id="company"
+            placeholder="Company"
+          ></input>
+        </div>
+        <div className="flex flex-col">
+          <label for="position">Position:</label>
+          <input
+            type="text"
+            className="rounded   pl-2 shadow border border-slate-400 focus:border-sky-500 focus:outline-none"
+            id="position"
+            placeholder="Position"
+          ></input>
+        </div>
       </div>
       <label for="from-date">From Date:</label>
       <input type="date" id="from-date"></input>
