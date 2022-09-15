e6307b16bd1f587aa7c7d280f51f5f14c1792826
Matan <matanbitton2@gmail.com>
2022-09-15 23:57:18 +0300

    add bullet point section

--- src/WorkExp.js
+++ src/WorkExp.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function WorkExp() {
+export default function WorkExp({ handleWorkExperience, workExperience }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-2 ">
       <div className="flex justify-evenly">
         <div className="flex flex-col">
           <label for="company">Company:</label>
@@ -11,6 +11,9 @@ export default function WorkExp() {
             className="rounded   pl-2 shadow border border-slate-400 focus:border-sky-500 focus:outline-none"
             id="company"
             placeholder="Company"
+            name="company"
+            onChange={handleWorkExperience}
+            value={workExperience.company}
           ></input>
         </div>
         <div className="flex flex-col">
@@ -20,14 +23,50 @@ export default function WorkExp() {
             className="rounded   pl-2 shadow border border-slate-400 focus:border-sky-500 focus:outline-none"
             id="position"
             placeholder="Position"
+            name="position"
+            onChange={handleWorkExperience}
+            value={workExperience.position}
           ></input>
         </div>
       </div>
-      <label for="from-date">From Date:</label>
-      <input type="date" id="from-date"></input>
-      <div className="flex">
-        <label for="to-date">To:</label>
-        <input type="date" id="to-date"></input>
+      <div className="flex justify-evenly">
+        <div className="flex flex-col">
+          <label for="from-date">From Date:</label>
+          <input
+            className="w-[258px] rounded shadow border text-slate-400  border-slate-400 focus:border-sky-500 focus:outline-none"
+            type="date"
+            id="from-date"
+            name="startDate"
+            onChange={handleWorkExperience}
+            value={workExperience.startDate}
+          ></input>
+        </div>
+        <div className="flex flex-col">
+          <label for="to-date">To Date:</label>
+          <input
+            className=" w-[258px] rounded shadow text-slate-400 border border-slate-400 focus:border-sky-500 focus:outline-none"
+            type="date"
+            id="to-date"
+            name="endDate"
+            onChange={handleWorkExperience}
+            value={workExperience.endDate}
+          ></input>
+        </div>
+      </div>
+      <div className="flex justify-center w-full">
+        <div className="flex flex-col w-[89%]">
+          <label for="bulletPoints">Job Description</label>
+          <input
+            className=" pl-2 w-full rounded shadow text-slate-400 border border-slate-400 focus:border-sky-500 focus:outline-none"
+            type="text"
+            id="bulletPoints"
+            placeholder="EXAMPLE: Increased company's revenue by..."
+            name="bulletPoints"
+            onChange={handleWorkExperience}
+            value={workExperience.bulletPoints}
+          ></input>
+          <button>Add Bullet Point</button>
+        </div>
       </div>
     </div>
   );
