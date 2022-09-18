21732b6ad4a084afa202705d45239008538c8b87
Matan <matanbitton2@gmail.com>
2022-09-18 18:18:16 +0300

    make responsive design breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: { min: "0px", max: "720px" },
+
+        md: { min: "720px", max: "1074px" },
+
+        lg: { min: "1074px", max: "100vw" },
+      },
+    },
+
+    plugins: [],
   },
-  plugins: [],
 };
