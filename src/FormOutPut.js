69119b866fda783d02d17c076cdb3033944d5cfd
Matan <matanbitton2@gmail.com>
2022-10-01 12:42:01 +0300

    change file name

---
+++ src/FormOutPut.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export default function Resume() {
+  return (
+    <div className=" shadow-lg  p-4 rounded-md   max-h-[1080px] sm:hidden md:hidden  lg:min-w-[750px] md:min-w-[550px] sm:min-w-[340px]    min-h-[950px] sm:max-w-[50%]  bg-gray-100 text-gray-800">
+      sadasdsa
+    </div>
+  );
+}
