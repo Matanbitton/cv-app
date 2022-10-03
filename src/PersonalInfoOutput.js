c0e3a034142851d0cc2bafe69db24c1d1fbb643f
Matan <matanbitton2@gmail.com>
2022-10-03 18:29:35 +0300

    add file and make UI

--- src/PersonalInfoOutput.js
+++ src/PersonalInfoOutput.js
@@ -8,9 +8,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function personalInfoOutput({ personalInfo }) {
   return (
-    <div className="flex flex-col p-2 justify-center items-center border-sky-500  bg-slate-800 ">
+    <div className="flex flex-col justify-center items-center  border-sky-500   ">
       {personalInfo.firstName && personalInfo.lastName ? (
-        <div className="flex gap-2 text-3xl text-sky-400 font-bold">
+        <div className="flex gap-2 text-3xl text-sky-400 font-bold bg-slate-800 px-2 pt-2 text-center justify-center w-full">
           <h1>{personalInfo.firstName}</h1>
           <h1>{personalInfo.lastName}</h1>
         </div>
@@ -18,38 +18,65 @@ export default function personalInfoOutput({ personalInfo }) {
         ""
       )}
 
-      <div className="flex gap-2 text-white">
-        <div className="flex justify-center items-center gap-1">
+      <div className="flex gap-2 text-white w-full bg-slate-800 items-center justify-center px-2 pb-1 text-center">
+        <div className="flex justify-center items-center gap-1 ">
           {personalInfo.phoneNumber && (
             <>
-              {" "}
               <div className="text-sm text-sky-600">
                 <FontAwesomeIcon icon={faPhone} />
               </div>
-              {personalInfo.phoneNumber}{" "}
+              <p className="break-all text-left">{personalInfo.phoneNumber}</p>
             </>
           )}
         </div>
-        <div className="flex justify-center items-center gap-1">
-          <div className="text-sm text-sky-600">
-            <FontAwesomeIcon icon={faEnvelope} />
+        <div className="flex gap-2 ">
+          <div className="flex justify-center items-center gap-1 ">
+            {personalInfo.email && (
+              <>
+                <div className="text-sm text-sky-600">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </div>
+                <p className="break-all text-left">{personalInfo.email}</p>
+              </>
+            )}
           </div>
-          {personalInfo.email}
-        </div>
-        <div className="flex justify-center items-center gap-1">
-          <div className="text-sm text-sky-600">
-            <FontAwesomeIcon icon={faLink} />
+          <div className="flex justify-center items-center gap-1  ">
+            {personalInfo.linkedinLink && (
+              <>
+                <div className="text-sm text-sky-600">
+                  <FontAwesomeIcon icon={faLink} />
+                </div>
+                <p className="break-all text-left">
+                  {" "}
+                  {personalInfo.linkedinLink}
+                </p>
+              </>
+            )}
           </div>
-          {personalInfo.linkedinLink}
-        </div>
-        <div className="flex justify-center items-center gap-1">
-          <div className="text-sm text-sky-600">
-            <FontAwesomeIcon icon={faGlobe} />
+          <div className="flex justify-center items-center gap-1 ">
+            {personalInfo.websiteLink && (
+              <>
+                <div className="text-sm text-sky-600">
+                  <FontAwesomeIcon icon={faGlobe} />
+                </div>
+                <p className="break-all text-left">
+                  {personalInfo.websiteLink}
+                </p>
+              </>
+            )}
           </div>
-          {personalInfo.websiteLink}
         </div>
       </div>
-      <div>{personalInfo.summarySection}</div>
+      {personalInfo.summarySection && (
+        <div className=" w-full  bg-gray-100 p-2  pl-3  ">
+          <h1 className=" text-sky-500 text-xl font-bold border-b-2 border-sky-500">
+            Summary
+          </h1>
+          <p className="text-black pl-2 py-1 break-all text-left">
+            {personalInfo.summarySection}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/WorkExpOutput.js
@@ -0,0 +1,61 @@
+import {
+  faEnvelope,
+  faGlobe,
+  faLink,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function WorkExpOutput({ workExpArr }) {
+  function workExpComponent() {}
+
+  return (
+    <div className="flex flex-col   border-sky-500 p-4 pl-3 ">
+      {workExpArr.length >= 1 ? (
+        <h1 className="font-medium  text-sky-500 text-lg border-b-2 border-sky-500">
+          Work Experience
+        </h1>
+      ) : (
+        ""
+      )}
+      {workExpArr.map((work) => (
+        <div className="w-full flex-col pl-3">
+          <div className="flex  justify-between">
+            <div className="flex gap-2">
+              <h1 className="font-medium ">{work.company}</h1>
+              <p>-</p>
+              <p className="italic ">{work.position}</p>
+            </div>
+            <div className="flex gap-2  items-center">
+              <p className="italic text-sm">{work.startDate}</p>
+              <p>-</p>
+              <p className="italic text-sm">{work.endDate}</p>
+            </div>
+          </div>
+          <div className="flex flex-col pl-3 ">
+            {work.bulletPoint1 ? (
+              <p className="text-slate-800">- {work.bulletPoint1}</p>
+            ) : (
+              ""
+            )}
+            {work.bulletPoint2 ? (
+              <p className="text-slate-800">- {work.bulletPoint2}</p>
+            ) : (
+              ""
+            )}
+            {work.bulletPoint3 ? (
+              <p className="text-slate-800">- {work.bulletPoint3}</p>
+            ) : (
+              ""
+            )}
+            {work.bulletPoint4 ? (
+              <p className="text-slate-800">- {work.bulletPoint4}</p>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
