085a6fb7a278320909bb52bd57479bf1030f2004
Matan <matanbitton2@gmail.com>
2022-10-05 14:32:33 +0300

    add file

---
+++ src/ProjectOutput.js
@@ -0,0 +1,49 @@
+import formatDate from "./formatDate";
+import uniqid from "uniqid";
+
+export default function WorkExpOutput({ projectsArr }) {
+  return (
+    <div className="flex flex-col   border-sky-500 p-4 pl-3 ">
+      {projectsArr.length >= 1 ? (
+        <h1 className="font-medium  text-sky-500 text-lg border-b-2 border-sky-500">
+          Projects
+        </h1>
+      ) : (
+        ""
+      )}
+      {projectsArr.map((project) => (
+        <div className="w-full flex-col pl-3" key={uniqid()}>
+          <div className="flex  justify-between">
+            <div className="flex gap-2">
+              <h1 className="font-medium ">{project.projectName}</h1>
+              <p>-</p>
+              <p className="italic ">{project.tech}</p>
+            </div>
+          </div>
+          <div className="flex flex-col pl-3 ">
+            {project.bulletPoint1 ? (
+              <p className="text-slate-800">- {project.bulletPoint1}</p>
+            ) : (
+              ""
+            )}
+            {project.bulletPoint2 ? (
+              <p className="text-slate-800">- {project.bulletPoint2}</p>
+            ) : (
+              ""
+            )}
+            {project.bulletPoint3 ? (
+              <p className="text-slate-800">- {project.bulletPoint3}</p>
+            ) : (
+              ""
+            )}
+            {project.bulletPoint4 ? (
+              <p className="text-slate-800">- {project.bulletPoint4}</p>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
