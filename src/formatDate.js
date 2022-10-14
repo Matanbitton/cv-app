eaa885a05326bba7aa188b994cc2116b0124a738
Matan <matanbitton2@gmail.com>
2022-10-14 16:05:41 +0300

    fix bug caused crash due to empty date string

--- src/formatDate.js
+++ src/formatDate.js
@@ -2,6 +2,9 @@ import format from "date-fns/format";
 import parseISO from "date-fns/parseISO";
 
 export default function formatDate(date) {
+  if (date === "") {
+    return "present";
+  }
   const dateFormated = format(parseISO(date), "MMMM yyyy");
 
   return dateFormated;
