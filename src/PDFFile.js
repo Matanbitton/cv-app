09fba1a6953790c402774d0b54529bfe509099d0
Matan <matanbitton2@gmail.com>
2022-10-17 19:32:21 +0300

    add file to print the CV

--- src/PDFFile.js
+++ src/PDFFile.js
@@ -1,53 +1,383 @@
-import React, { useEffect, useState } from "react";
-import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
-import { PDFViewer } from "@react-pdf/renderer";
+import React from "react";
+import {forwardRef} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faGlobe,
+  faLink,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+import uniqid from "uniqid";
+import formatDate from "./formatDate";
 
-const styles = StyleSheet.create({
-  page: {
-    flexDirection: "row",
-    backgroundColor: "#E4E4E4",
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
-  },
-  name: {
-    backgroundColor: "#1e293b",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    width: "100%",
-    height: "10%",
-    color: "#0ea5e9",
-  },
-});
+const PDFFile = forwardRef(({personalInfo, workExpArr, educationArr, projectsArr, custom, skills},ref) => {
+    return (
+      <div ref={ref} className="bg-gray-100 h-screen">
+        <div className="flex flex-col justify-center items-center  border-sky-500   ">
+          {personalInfo.firstName && personalInfo.lastName ? (
+            <div className="flex gap-2 text-3xl text-sky-400 font-bold bg-slate-800 px-2 pt-2 text-center justify-center w-full">
+              <h1>{personalInfo.firstName}</h1>
+              <h1>{personalInfo.lastName}</h1>
+            </div>
+          ) : (
+            ""
+          )}
 
-// Create Document Component
-function PDFFile({ personalInfo }) {
-  return (
-    <Document>
-      <Page size="A4" style={styles.page}>
-        <View style={styles.name}>
-          <Text>{`blahblah`}</Text>
-          <Text>{`blibli`}</Text>
-        </View>
-      </Page>
-    </Document>
-  );
-}
+          <div className="flex gap-2 text-white w-full bg-slate-800 items-center justify-center px-2 pb-1 text-center">
+            <div className="flex justify-center items-center gap-1 ">
+              {personalInfo.phoneNumber && (
+                <>
+                  <div className="text-sm text-sky-600">
+                    {" "}
+                    <FontAwesomeIcon icon={faPhone} />
+                  </div>
 
-const PDFView = () => {
-  const [client, setClient] = useState(false);
+                  <p className="break-all text-left">
+                    {personalInfo.phoneNumber}
+                  </p>
+                </>
+              )}
+            </div>
+            <div className="flex gap-2 ">
+              <div className="flex justify-center items-center gap-1 ">
+                {personalInfo.email && (
+                  <>
+                    <div className="text-sm text-sky-600">
+                      {" "}
+                      <FontAwesomeIcon icon={faEnvelope} />
+                    </div>
 
-  useEffect(() => {
-    setClient(true);
-  }, []);
+                    <p className="break-all text-left">{personalInfo.email}</p>
+                  </>
+                )}
+              </div>
+              <div className="flex justify-center items-center gap-1  ">
+                {personalInfo.linkedinLink && (
+                  <>
+                    <div className="text-sm text-sky-600">
+                      {" "}
+                      <FontAwesomeIcon icon={faLink} />
+                    </div>
 
-  return (
-    <PDFViewer>
-      <PDFFile />
-    </PDFViewer>
-  );
-};
-export default PDFView;
+                    <a
+                      className=" text-left"
+                      href={personalInfo.linkedinLink}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Linkedin
+                    </a>
+                  </>
+                )}
+              </div>
+              <div className="flex justify-center items-center gap-1 ">
+                {personalInfo.websiteLink && (
+                  <>
+                    <div className="text-sm text-sky-600">
+                      <FontAwesomeIcon icon={faGlobe} />
+                    </div>
+                    <a
+                      className=" text-left"
+                      href={personalInfo.websiteLink}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Portfolio
+                    </a>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+          {personalInfo.summarySection && (
+            <div className=" w-full  px-3 pt-2  ">
+              <h1 className=" font-medium  text-sky-500 text-xl border-b-2 border-sky-500">
+                Summary
+              </h1>
+              <p className="text-slate-800 pl-2 py-1 break  text-left">
+                {personalInfo.summarySection}
+              </p>
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col   border-sky-500  px-3 " ref={ref}>
+          {workExpArr.length >= 1 ? (
+            <h1 className="font-medium  text-sky-500 text-lg border-b-2 border-sky-500">
+              Work Experience
+            </h1>
+          ) : (
+            ""
+          )}
+          {workExpArr.map((work) => (
+            <div className="w-full flex-col pl-3" key={uniqid()}>
+              <div className="flex  justify-between">
+                <div className="flex gap-2">
+                  <h1 className="font-medium ">{work.company}</h1>
+                  <p>-</p>
+                  <p className="italic ">{work.position}</p>
+                </div>
+                <div className="flex gap-2  items-center">
+                  <p className="italic text-sm">{formatDate(work.startDate)}</p>
+                  <p>-</p>
+                  <p className="italic text-sm">{formatDate(work.endDate)}</p>
+                </div>
+              </div>
+              <div className="flex flex-col pl-3 ">
+                {work.bulletPoint1 ? (
+                  <p className="text-slate-800">- {work.bulletPoint1}</p>
+                ) : (
+                  ""
+                )}
+                {work.bulletPoint2 ? (
+                  <p className="text-slate-800">- {work.bulletPoint2}</p>
+                ) : (
+                  ""
+                )}
+                {work.bulletPoint3 ? (
+                  <p className="text-slate-800">- {work.bulletPoint3}</p>
+                ) : (
+                  ""
+                )}
+                {work.bulletPoint4 ? (
+                  <p className="text-slate-800">- {work.bulletPoint4}</p>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col   border-sky-500 px-3 ">
+          {educationArr.length >= 1 ? (
+            <h1 className="font-medium  text-sky-500 text-lg border-b-2 border-sky-500">
+              Education
+            </h1>
+          ) : (
+            ""
+          )}
+          {educationArr.map((education) => (
+            <div className="w-full flex-col pl-3" key={uniqid()}>
+              <div className="flex  justify-between">
+                <div className="flex gap-2">
+                  <h1 className="font-medium ">{education.institution}</h1>
+                  <p>-</p>
+                  <p className="italic ">{education.degree}</p>
+                </div>
+                <div className="flex gap-2  items-center">
+                  <p className="italic text-sm">
+                    {formatDate(education.startDate)}
+                  </p>
+                  <p>-</p>
+                  <p className="italic text-sm">
+                    {formatDate(education.endDate)}
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col pl-3 ">
+                {education.bulletPoint1 ? (
+                  <p className="text-slate-800">- {education.bulletPoint1}</p>
+                ) : (
+                  ""
+                )}
+                {education.bulletPoint2 ? (
+                  <p className="text-slate-800">- {education.bulletPoint2}</p>
+                ) : (
+                  ""
+                )}
+                {education.bulletPoint3 ? (
+                  <p className="text-slate-800">- {education.bulletPoint3}</p>
+                ) : (
+                  ""
+                )}
+                {education.bulletPoint4 ? (
+                  <p className="text-slate-800">- {education.bulletPoint4}</p>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col  gap-2 border-sky-500 px-3 ">
+          {projectsArr.length >= 1 ? (
+            <h1 className="font-medium  text-sky-500 text-lg border-b-2 border-sky-500">
+              Projects
+            </h1>
+          ) : (
+            ""
+          )}
+          {projectsArr.map((project) => (
+            <div className="w-full flex-col pl-3" key={uniqid()}>
+              <div className="flex  justify-between">
+                <div className="flex gap-2 items-center">
+                  <h1 className="font-medium ">{project.projectName}</h1>
+                  <p>-</p>
+                  <p className="italic text-sm">{project.tech}</p>
+                </div>
+              </div>
+              <div className="flex flex-col pl-3 ">
+                {project.bulletPoint1 ? (
+                  <p className="text-slate-800">- {project.bulletPoint1}</p>
+                ) : (
+                  ""
+                )}
+                {project.bulletPoint2 ? (
+                  <p className="text-slate-800">- {project.bulletPoint2}</p>
+                ) : (
+                  ""
+                )}
+                {project.bulletPoint3 ? (
+                  <p className="text-slate-800">- {project.bulletPoint3}</p>
+                ) : (
+                  ""
+                )}
+                {project.bulletPoint4 ? (
+                  <p className="text-slate-800">- {project.bulletPoint4}</p>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col   border-sky-500 px-3">
+          {custom.sectionTitle ? (
+            <h1 className="font-medium  text-sky-500 text-lg border-b-2 border-sky-500">
+              {custom.sectionTitle}
+            </h1>
+          ) : (
+            ""
+          )}
+
+          <div className="w-full flex-col pl-3" key={uniqid()}>
+            <div className="flex  justify-between">
+              <div className="flex gap-2">
+                <h1 className="font-medium ">{custom.header}</h1>
+              </div>
+              {custom.startDate && custom.endDate ? (
+                <div className="flex gap-2  items-center">
+                  <p className="italic text-sm">
+                    {formatDate(custom.startDate)}
+                  </p>
+                  <p>-</p>
+                  <p className="italic text-sm">{formatDate(custom.endDate)}</p>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+            <div className="flex flex-col pl-3 ">
+              {custom.bulletPoint1 ? (
+                <p className="text-slate-800">- {custom.bulletPoint1}</p>
+              ) : (
+                ""
+              )}
+              {custom.bulletPoint2 ? (
+                <p className="text-slate-800">- {custom.bulletPoint2}</p>
+              ) : (
+                ""
+              )}
+              {custom.bulletPoint3 ? (
+                <p className="text-slate-800">- {custom.bulletPoint3}</p>
+              ) : (
+                ""
+              )}
+              {custom.bulletPoint4 ? (
+                <p className="text-slate-800">- {custom.bulletPoint4}</p>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col   border-sky-500 px-3 ">
+          {skills.skill1 !== "" ? (
+            <h1 className="font-medium  text-sky-500 text-lg border-b-2 border-sky-500">
+              Skills
+            </h1>
+          ) : (
+            ""
+          )}
+
+          <div className="w-full flex-col py-2" key={uniqid()}>
+            <div className="flex pl-2 gap-2 flex-wrap">
+              {skills.skill1 ? (
+                <p className="text-white bg-slate-800 rounded-md px-2">
+                  {skills.skill1}
+                </p>
+              ) : (
+                ""
+              )}
+              {skills.skill2 ? (
+                <p className="text-white bg-slate-800 rounded-md px-2">
+                  {skills.skill2}
+                </p>
+              ) : (
+                ""
+              )}
+              {skills.skill3 ? (
+                <p className="text-white bg-slate-800 rounded-md px-2">
+                  {skills.skill3}
+                </p>
+              ) : (
+                ""
+              )}
+
+              {skills.skill4 ? (
+                <p className="text-white bg-slate-800 rounded-md px-2">
+                  {skills.skill4}
+                </p>
+              ) : (
+                ""
+              )}
+              {skills.skill5 ? (
+                <p className="text-white bg-slate-800 rounded-md px-2">
+                  {skills.skill5}
+                </p>
+              ) : (
+                ""
+              )}
+              {skills.skill6 ? (
+                <p className="text-white bg-slate-800 rounded-md px-2">
+                  {skills.skill6}
+                </p>
+              ) : (
+                ""
+              )}
+              {skills.skill7 ? (
+                <p className="text-white bg-slate-800 rounded-md px-2">
+                  {skills.skill7}
+                </p>
+              ) : (
+                ""
+              )}
+              {skills.skill8 ? (
+                <p className="text-white bg-slate-800 rounded-md px-2">
+                  {skills.skill8}
+                </p>
+              ) : (
+                ""
+              )}
+              {skills.skill9 ? (
+                <p className="text-white bg-slate-800 rounded-md px-2">
+                  {skills.skill9}
+                </p>
+              ) : (
+                ""
+              )}
+              {skills.skill10 ? (
+                <p className="text-white bg-slate-800 rounded-md px-2">
+                  {skills.skill10}
+                </p>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+);
+
+export default PDFFile;
