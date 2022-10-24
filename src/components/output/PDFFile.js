8fcac4c77d3f2bf082b5b072d4e981026d67ec95
Matan <matanbitton2@gmail.com>
2022-10-24 12:12:38 +0300

    add new file

--- src/components/output/PDFFile.js
+++ src/components/output/PDFFile.js
@@ -108,6 +108,60 @@ const PDFFile = forwardRef(
             </div>
           )}
         </div>
+        <div className="flex flex-col  gap-2 border-sky-500 px-3 ">
+          {projectsArr.length >= 1 ? (
+            <h1 className="font-medium  text-sky-500 text-lg border-b-2 border-sky-500">
+              Projects
+            </h1>
+          ) : (
+            ""
+          )}
+          {projectsArr.map((project) => (
+            <div className="w-full flex-col pl-3" key={uniqid()}>
+              <div className="flex  justify-between">
+                <div className="flex gap-2 items-center">
+                  <h1 className="font-medium ">{project.projectName}</h1>
+                  <p>-</p>
+                  <p className="italic text-sm">{project.tech}</p>
+                </div>
+              </div>
+              <div className="flex flex-col pl-3 ">
+                {project.bulletPoint1 ? (
+                  <div className="flex gap-1">
+                    <p>•</p>
+                    <p className="text-slate-800">{project.bulletPoint1}</p>
+                  </div>
+                ) : (
+                  ""
+                )}
+                {project.bulletPoint2 ? (
+                  <div className="flex gap-1">
+                    <p>•</p>
+                    <p className="text-slate-800">{project.bulletPoint2}</p>
+                  </div>
+                ) : (
+                  ""
+                )}
+                {project.bulletPoint3 ? (
+                  <div className="flex gap-1">
+                    <p>•</p>
+                    <p className="text-slate-800">{project.bulletPoint3}</p>
+                  </div>
+                ) : (
+                  ""
+                )}
+                {project.bulletPoint4 ? (
+                  <div className="flex gap-1">
+                    <p>•</p>
+                    <p className="text-slate-800">{project.bulletPoint4}</p>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
         <div className="flex flex-col gap-2 border-sky-500  px-3 ">
           {workExpArr.length >= 1 ? (
             <h1 className="font-medium  text-sky-500 text-lg border-b-2 border-sky-500">
@@ -166,6 +220,69 @@ const PDFFile = forwardRef(
               </div>
             </div>
           ))}
+        </div>
+
+        <div className="flex flex-col   border-sky-500 px-3">
+          {custom.sectionTitle ? (
+            <h1 className="font-medium  text-sky-500 text-lg border-b-2 border-sky-500">
+              {custom.sectionTitle}
+            </h1>
+          ) : (
+            ""
+          )}
+
+          <div className="w-full flex-col pl-3" key={uniqid()}>
+            <div className="flex  justify-between">
+              <div className="flex gap-2">
+                <h1 className="font-medium ">{custom.header}</h1>
+              </div>
+              {custom.startDate && custom.endDate ? (
+                <div className="flex gap-2  items-center">
+                  <p className="italic text-sm">
+                    {formatDate(custom.startDate)}
+                  </p>
+                  <p>-</p>
+                  <p className="italic text-sm">{formatDate(custom.endDate)}</p>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+            <div className="flex flex-col pl-3 ">
+              {custom.bulletPoint1 ? (
+                <div className="flex gap-1">
+                  <p>•</p>
+                  <p className="text-slate-800">{custom.bulletPoint1}</p>
+                </div>
+              ) : (
+                ""
+              )}
+              {custom.bulletPoint2 ? (
+                <div className="flex gap-1">
+                  <p>•</p>
+                  <p className="text-slate-800">{custom.bulletPoint2}</p>
+                </div>
+              ) : (
+                ""
+              )}
+              {custom.bulletPoint3 ? (
+                <div className="flex gap-1">
+                  <p>•</p>
+                  <p className="text-slate-800">{custom.bulletPoint3}</p>
+                </div>
+              ) : (
+                ""
+              )}
+              {custom.bulletPoint4 ? (
+                <div className="flex gap-1">
+                  <p>•</p>
+                  <p className="text-slate-800">{custom.bulletPoint4}</p>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
         </div>
         <div className="flex flex-col gap-2 border-sky-500 px-3 ">
           {educationArr.length >= 1 ? (
@@ -229,122 +346,6 @@ const PDFFile = forwardRef(
               </div>
             </div>
           ))}
-        </div>
-        <div className="flex flex-col  gap-2 border-sky-500 px-3 ">
-          {projectsArr.length >= 1 ? (
-            <h1 className="font-medium  text-sky-500 text-lg border-b-2 border-sky-500">
-              Projects
-            </h1>
-          ) : (
-            ""
-          )}
-          {projectsArr.map((project) => (
-            <div className="w-full flex-col pl-3" key={uniqid()}>
-              <div className="flex  justify-between">
-                <div className="flex gap-2 items-center">
-                  <h1 className="font-medium ">{project.projectName}</h1>
-                  <p>-</p>
-                  <p className="italic text-sm">{project.tech}</p>
-                </div>
-              </div>
-              <div className="flex flex-col pl-3 ">
-                {project.bulletPoint1 ? (
-                  <div className="flex gap-1">
-                    <p>•</p>
-                    <p className="text-slate-800">{project.bulletPoint1}</p>
-                  </div>
-                ) : (
-                  ""
-                )}
-                {project.bulletPoint2 ? (
-                  <div className="flex gap-1">
-                    <p>•</p>
-                    <p className="text-slate-800">{project.bulletPoint2}</p>
-                  </div>
-                ) : (
-                  ""
-                )}
-                {project.bulletPoint3 ? (
-                  <div className="flex gap-1">
-                    <p>•</p>
-                    <p className="text-slate-800">{project.bulletPoint3}</p>
-                  </div>
-                ) : (
-                  ""
-                )}
-                {project.bulletPoint4 ? (
-                  <div className="flex gap-1">
-                    <p>•</p>
-                    <p className="text-slate-800">{project.bulletPoint4}</p>
-                  </div>
-                ) : (
-                  ""
-                )}
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="flex flex-col   border-sky-500 px-3">
-          {custom.sectionTitle ? (
-            <h1 className="font-medium  text-sky-500 text-lg border-b-2 border-sky-500">
-              {custom.sectionTitle}
-            </h1>
-          ) : (
-            ""
-          )}
-
-          <div className="w-full flex-col pl-3" key={uniqid()}>
-            <div className="flex  justify-between">
-              <div className="flex gap-2">
-                <h1 className="font-medium ">{custom.header}</h1>
-              </div>
-              {custom.startDate && custom.endDate ? (
-                <div className="flex gap-2  items-center">
-                  <p className="italic text-sm">
-                    {formatDate(custom.startDate)}
-                  </p>
-                  <p>-</p>
-                  <p className="italic text-sm">{formatDate(custom.endDate)}</p>
-                </div>
-              ) : (
-                ""
-              )}
-            </div>
-            <div className="flex flex-col pl-3 ">
-              {custom.bulletPoint1 ? (
-                <div className="flex gap-1">
-                  <p>•</p>
-                  <p className="text-slate-800">{custom.bulletPoint1}</p>
-                </div>
-              ) : (
-                ""
-              )}
-              {custom.bulletPoint2 ? (
-                <div className="flex gap-1">
-                  <p>•</p>
-                  <p className="text-slate-800">{custom.bulletPoint2}</p>
-                </div>
-              ) : (
-                ""
-              )}
-              {custom.bulletPoint3 ? (
-                <div className="flex gap-1">
-                  <p>•</p>
-                  <p className="text-slate-800">{custom.bulletPoint3}</p>
-                </div>
-              ) : (
-                ""
-              )}
-              {custom.bulletPoint4 ? (
-                <div className="flex gap-1">
-                  <p>•</p>
-                  <p className="text-slate-800">{custom.bulletPoint4}</p>
-                </div>
-              ) : (
-                ""
-              )}
-            </div>
-          </div>
         </div>
         <div className="flex flex-col   border-sky-500 px-3 ">
           {skills.skill1 !== "" ? (
