bf82debdc528356e7d2c4451e1dc9202b514d1e1
Matan <matanbitton2@gmail.com>
2022-10-28 12:25:28 +0300

    fix bug

--- src/components/UI/SuccessModal.js
+++ src/components/UI/SuccessModal.js
@@ -25,7 +25,7 @@ const SuccessModal = ({message}) => {
             animate={{y: 0, x: "-50%"}}
             initial={{y: -100, x: "-50%"}}
             transition={{ease: "easeIn", duration: 0.2}}
-            className=" fixed left-[50%] translate-x-[-50%] w-[90%] flex items-center justify-center text-white font-semibold  sm:top-4 md:bottom-12 gap-2 rounded-lg lg:bottom-2  bg-sky-500 border-2 px-2 py-2 shadow-xl"
+            className=" fixed left-[50%] translate-x-[-50%] w-[90%] flex items-center justify-center text-white font-semibold  sm:top-2 md:bottom-12 gap-2 rounded-lg lg:bottom-2  bg-sky-500 border-2 px-2 py-2 shadow-xl"
           >
             <FontAwesomeIcon icon={faThumbsUp} />
             <p>{message}</p>
