a6e6051ef05e25a4f9a65c9b8a3eada4f1acf4a6
Matan <matanbitton2@gmail.com>
2022-11-04 00:06:12 +0200

    add context folder

---
+++ src/store/theme-context.js
@@ -0,0 +1,11 @@
+import React, {createContext} from "react";
+
+const ThemeContext = createContext({
+  theme: {
+    bg: "bg-slate-800",
+    text: "text-sky-400",
+    icons: "text-sky-600",
+  },
+});
+
+export default ThemeContext;
